11361e6282cc332c117afe8b8e8318230c48c393
signaturecoder <snkmr13@gmail.com>
2021-03-16 22:55:44 +0530

    designing of pizza card

--- src/components/PizzaCard.jsx
+++ src/components/PizzaCard.jsx
@@ -1,73 +1,110 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import CardMedia from '@material-ui/core/CardMedia';
-import Box from '@material-ui/core/Box';
-import Rating from '@material-ui/lab/Rating';
-import Button from '@material-ui/core/Button';
-import PizzaDetails from './PizzaDetails';
-
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardMedia from "@material-ui/core/CardMedia";
+import Box from "@material-ui/core/Box";
+import Rating from "@material-ui/lab/Rating";
+import Button from "@material-ui/core/Button";
+import PizzaDetails from "./PizzaDetails";
+import StarRateIcon from "@material-ui/icons/StarRate";
 const useStyles = makeStyles({
-    card: {
-      maxWidth: 345,
-      marginTop: 40,
-      boxShadow: ' 5px 5px 15px 5px #888888',
-    },
-    media: {
-      height: 200,
-    },
-    removeLine: {
-      textDecoration: 'none',
-      color: 'white',
-    },
-  });
+  card: {
+    maxWidth: 345,
+    width: 345,
+    marginTop: 40,
+    boxShadow: " 5px 5px 15px 5px #888888",
+  },
+  media: {
+    height: 200,
+  },
+  description: {
+    color: "#686b78",
+    fontSize: 12,
+  },
+  rating: {
+    backgroundColor: "#48c479",
+    color: "#fff",
+    fontWeight: 500,
+    width: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  removeLine: {
+    textDecoration: "none",
+    color: "white",
+  },
+  subContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+});
 
-const PizzaCard = ({name, description, price, rating, size, toppings, imgSrc}) => {
+const PizzaCard = ({
+  name,
+  description,
+  price,
+  rating,
+  size,
+  toppings,
+  imgSrc,
+}) => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState("");
+  const handleAdd = () => {
+    setOpen(true);
+  };
 
-    const classes = useStyles();
-    const [open, setOpen] = useState(false);
-    const [selectedValue, setSelectedValue] = useState("");
-    const handleAdd = () => {
-         setOpen(true);
-    }
+  const handleClose = (value) => {
+    setOpen(false);
+    setSelectedValue(value);
+  };
 
-    const handleClose = (value) => {
-      setOpen(false);
-      setSelectedValue(value);
-    };
-
-   
-return (
+  return (
     <Card className={classes.card}>
-    <CardActionArea>
-      <CardMedia
-        className={classes.media}
-        image={imgSrc}
-        title="pizza"
+ 
+        <CardMedia className={classes.media} image={imgSrc} title="pizza" />
+        <CardContent>
+          <Typography variant="h6" gutterBottom>
+            {name}
+            <Box textAlign="left" className={classes.description}>
+              {description}
+            </Box>
+          </Typography>
+          <Typography
+            className={classes.subContainer}
+            variant="body2"
+            gutterBottom
+          >
+            <Box component="span">&#x20B9; {price}</Box>
+            <Box component="span" className={classes.rating}>
+              <StarRateIcon />
+              {rating}
+            </Box>
+            <Button
+              size="small"
+              variant="outlined"
+              color="primary"
+              onClick={handleAdd}
+            >
+              Add{" "}
+            </Button>
+          </Typography>
+        </CardContent>
+      <PizzaDetails
+        selectedValue={selectedValue}
+        open={open}
+        onClose={handleClose}
+        size={size}
+        toppings={toppings}
       />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-        {name}
-        </Typography>
-        <Typography variant="h6" component="h6">
-          <Box textAlign="left">Desciption : {description}</Box>
-          <Box textAlign="left">Price : Rs { price}</Box>
-          <Box textAlign="left">
-            Rating : <Rating name="read-only" value={rating} readOnly />
-          </Box>
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-    <CardActions>
-    <Button variant="contained" color="primary" onClick={handleAdd}>Add </Button>
-    </CardActions>
-    <PizzaDetails selectedValue={selectedValue} open={open} onClose={handleClose} size={size} toppings={toppings}/>
-  </Card>
-)
-}
+    </Card>
+  );
+};
 
 export default PizzaCard;
--- src/containers/PizzaList.jsx
+++ src/containers/PizzaList.jsx
@@ -1,48 +1,126 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import PizzaCard from '../components/PizzaCard';
-import { makeStyles } from '@material-ui/core/styles';
-import { useDispatch, useSelector } from 'react-redux';
-import {loadPizzas} from '../redux/actions/pizzaAction'
-const useStyles = makeStyles({
-    cardList: {
-     display: 'flex',
-     flexWrap: 'wrap',
-     justifyContent: 'space-around'
-    },
-  });
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import PizzaCard from "../components/PizzaCard";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Switch from "@material-ui/core/Switch";
+import { useDispatch, useSelector } from "react-redux";
+import { loadPizzas } from "../redux/actions/pizzaAction";
+const useStyles = makeStyles((theme) => ({
+  cardList: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
+  menu: {
+    display: "flex",
+    justifyContent: "space-around",
+    background: "#ccc",
+    width: "100%",
+    height: "25%",
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
 
 const PizzaList = () => {
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const pizzaList = useSelector((state) => state.pizzas);
-    // const [pizzaList, setPizzaList] = useState([])
-    useEffect(() => {
-        // axios.get('https://run.mocky.io/v3/ec196a02-aaf4-4c91-8f54-21e72f241b68').then(res => {
-        //     console.log(res);
-        //     const pizzaListRes = res.data;
-        //     setPizzaList(pizzaListRes);
-        // })
-        dispatch(loadPizzas())
-    }, [])
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const pizzaList = useSelector((state) => state.pizzas);
+  const [isVeg, setIsVeg] = useState(false);
+  const [order, setOrder] = useState("asc");
+  const [ratingOrder, setRatingOrder] = useState("low");
+  // const [filteredPizzaList, setFilteredPizzaList] = useState(pizzaList);
+  const handleChange = (event) => {
+    setIsVeg(event.target.checked);
+    const filteredList = pizzaList.filter((pizza) => pizza.isVeg);
+    console.log("filterd", filteredList);
+    // setFilteredPizzaList(filteredList);
+  };
 
-    console.log('Pizzas', pizzaList);
-    return (
-        <div className={classes.cardList}>
-            {pizzaList && pizzaList.map(pizza => {
-               return  <PizzaCard 
-               key={pizza.id}
-               name={pizza.name} 
-               description={pizza.description}
-               imgSrc={pizza.img_url}
-               price={pizza.price}
-               rating={pizza.rating}
-               size={pizza.size}
-               toppings={pizza.toppings}
-               />
-            })}
-        </div>
-    )
-}
+  useEffect(() => {
+    dispatch(loadPizzas());
+  }, []);
+
+  //   console.log("Pizzas", filteredPizzaList);
+  return (
+    <div>
+      <div className={classes.menu}>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={isVeg}
+              onChange={handleChange}
+              name="checkedB"
+              color="primary"
+            />
+          }
+          label="Veg"
+          labelPlacement="start"
+        />
+
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-simple-select-outlined-label">
+            Sort By Price
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-outlined-label"
+            id="demo-simple-select-outlined"
+            value={order}
+            onChange={handleChange}
+            label="Sort By"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={"asc"}>Price: Low to High</MenuItem>
+            <MenuItem value={"desc"}>Price: High to Low</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl  className={classes.formControl}>
+          <InputLabel id="demo-simple-select-outlined-label">
+            Sort By Rating
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-outlined-label"
+            id="demo-simple-select-outlined"
+            value={ratingOrder}
+            onChange={handleChange}
+            label="Sort By"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={"low"}>Rating: Low to High</MenuItem>
+            <MenuItem value={"high"}>Rating: High to Low</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div className={classes.cardList}>
+        {pizzaList &&
+          pizzaList.map((pizza) => {
+            return (
+              <PizzaCard
+                key={pizza.id}
+                name={pizza.name}
+                description={pizza.description}
+                imgSrc={pizza.img_url}
+                price={pizza.price}
+                rating={pizza.rating}
+                size={pizza.size}
+                toppings={pizza.toppings}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
+};
 
 export default PizzaList;
